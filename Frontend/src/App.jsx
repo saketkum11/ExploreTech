b8f30219f89ccf065f3767f6dc58319df126b7a8
Saket kumar <kumarsaket601@gmail.com>
2023-11-18 14:01:24 +0530

    sends:connected backend to frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-
+import axios from "axios";
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/beer")
+      .then((res) => {
+        setData(res.data);
+        console.log(res);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <>
       <div>
         <h1>Connecting Backend to Frontend</h1>
+        {data.map((item, index) => {
+          return (
+            <>
+              <div key={`${item.value}`}>
+                <span>{item.color}</span>
+                <span>{item.value}</span>
+              </div>
+            </>
+          );
+        })}
       </div>
     </>
   );
